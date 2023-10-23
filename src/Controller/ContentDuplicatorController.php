8944dac9929204d9992e2ef649d632f12c1a650a
vystii <ngahabanda@gmail.com>
2023-10-23 15:53:25 +0100

    première approche

--- src/Controller/ContentDuplicatorController.php
+++ src/Controller/ContentDuplicatorController.php
@@ -43,8 +43,8 @@ class ContentDuplicatorController extends ControllerBase {
    */
   public function build($node) {
     $entity = $this->entityTypeManager->getStorage('node')->load($node);
-    
-    $newEntity = $entity->createDuplicate(); 
+
+    $newEntity = $entity->createDuplicate();
     $newEntity->setTitle($newEntity->getTitle() . " Clone");
     $newEntity->save();
 
@@ -57,15 +57,85 @@ class ContentDuplicatorController extends ControllerBase {
    * Builds the response.
    */
   public function buildSite($site_internet_entity) {
+    /**
+     * @var \Drupal\creation_site_virtuel\Entity\SiteInternetEntity
+     */
     $entity = $this->entityTypeManager->getStorage('site_internet_entity')->load($site_internet_entity);
-    
-    $newEntity = $entity->createDuplicate(); 
+    // dd([$entity->referencedEntities()[3]->toArray(), $entity->getFieldDefinitions(), $entity->toArray()]);
+    $newEntity = $this->duplicateEntity($entity);
+    // dd($entity->toArray());
+    // $newEntity = $entity->createDuplicate();
     $newEntity->setName($newEntity->getName() . " Clone");
-    $newEntity->save();
+    // $newEntity->enforceIsNew();
+    // $newEntity->save();
 
     $destination = $newEntity->toUrl();
 
     return $this->redirect($destination->getRouteName(), $destination->getRouteParameters());
   }
 
+  /**
+   * @param \Drupal\Core\Entity\EntityBase $entity
+   * @param boolean $is_sub
+   * @return boolean
+   */
+  public function duplicateEntity($entity, $is_sub = false) {
+    $newEntity = $entity->createDuplicate();
+    // dump($class_name);
+    // $newEntity->__construct($newEntity->toArray(), $class_name);
+    $ref_entities = $entity->referencedEntities();
+    dd($ref_entities);
+    // dd($ref_entities[0]);
+    $refs_values = [];
+    // dump($ref_entities);
+    // dump($entity->getFieldsDefinitions());
+    $new_field_values = [];
+    for ($i = 0; $i < count($ref_entities); $i++) {
+      // dump([ $ref_entities[$i]->getEntityTypeId(), $ref_entities[$i]->id()]);
+      $ent_id = $ref_entities[$i]->getEntityTypeId();
+      if ($ent_id != "user") {
+        $fields_names =  $this->getFieldKey($entity->toArray(), $ref_entities[$i]->id());
+        foreach ($fields_names as $name) {
+          $field_entity_id = $entity->get($name)->getFieldDefinition()->getItemDefinition()->toArray()["settings"]["target_type"];
+          if ($field_entity_id == $ref_entities[$i]->getEntityTypeId()) {
+            $new_id = $this->duplicateEntity($ref_entities[$i], true);
+            // $new_id = 3;
+            if (isset($refs_values[$name])) {
+              $refs_values[$name][] = $new_id;
+            } else {
+              $refs_values[$name] = [$new_id];
+            }
+            break;
+          }
+        }
+        // dump($newEntity->get($field_name[0])->getValue());
+        // dump([$ref_entities[$i]->getEntityTypeId(), $entity->get($field_name[0])->getFieldDefinition()->getItemDefinition()->toArray()["settings"]["target_type"]]);
+      }
+    }
+    foreach ($refs_values as $key => $value) {
+      $newEntity->set($key, $value);
+    }
+
+    // dd($refs_values, $newEntity->toArray());
+    $newEntity->save();
+    return $is_sub ? $newEntity->id() : $newEntity;
+  }
+
+  /**
+   * @param array $valueList
+   * @param int $entity_id
+   * @return array<string>
+   */
+  protected function getFieldKey($valueList, $entity_id) {
+    $result = [];
+    foreach ($valueList as $key => $field) {
+      foreach ($field as $field_item) {
+        if (isset($field_item["target_id"]) && $entity_id == $field_item["target_id"]) {
+          $result[] = $key;
+          break;
+        }
+      }
+    }
+    return $result;
+  }
 }
